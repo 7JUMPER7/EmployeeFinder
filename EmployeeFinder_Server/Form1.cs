7fe384dd5dc9b964f57e2406025f08617c8837d5
Grig_dm4i@student.itstep.org <Grig_dm4i@student.itstep.org>
2021-04-07 10:40:51 +0300

    Убрані все ошибки в сервере

--- EmployeeFinder_Server/Form1.cs
+++ EmployeeFinder_Server/Form1.cs
@@ -1,26 +1,19 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ToDoListServer
+namespace EmployeeFinder_Server
 {
     public partial class Form1 : Form
     {
         private IPAddress IpAddress;
         private int Port;
-        private DataBaseContext dbContext;
         private TcpListener tcpListener;
 
         private int DataGridCounter = 0;
@@ -28,10 +21,7 @@ namespace ToDoListServer
         public Form1()
         {
             InitializeComponent();
-            dbContext = new DataBaseContext();
-            DrawDataGrid();
         }
-
 
         //События
         private void StartBut_Click(object sender, EventArgs e)
@@ -49,23 +39,22 @@ namespace ToDoListServer
             thread.Start();
             WriteToConsole("Server started.");
         }
+
         private void leftBut_Click(object sender, EventArgs e)
         {
             if (DataGridCounter > 0)
             {
                 DataGridCounter--;
-                UpdateDataGrid();
             }
         }
+
         private void rightBut_Click(object sender, EventArgs e)
         {
             if (DataGridCounter < 2)
             {
                 DataGridCounter++;
-                UpdateDataGrid();
             }
         }
-
 
         //Ожидание запросов
         private void ListenerMethod()
@@ -86,161 +75,30 @@ namespace ToDoListServer
                     {
                         case "LGIN": //LogIn
                             {
-                                string[] info = data.Substring(5).Split(' ');
-                                string login = info[0];
-                                string pass = info[1];
-
-                                Classes.User user = null;
-                                foreach (Classes.User item in dbContext.Users)
-                                {
-                                    if (item.Login == login)
-                                    {
-                                        user = item;
-                                        break;
-                                    }
-                                }
-                                if (user != null) //Если логин найден
-                                {
-                                    if (user.Password == pass) //Если пароль совпадает
-                                    {
-                                        WriteToStream(tcpClient.GetStream(), "OK");
-                                        WriteToConsole($"{login}: Loged in.");
-                                    }
-                                    else //Если пароль не совпадает
-                                    {
-                                        WriteToStream(tcpClient.GetStream(), "PASS");
-                                        WriteToConsole($"{login}: Wrong password.");
-                                    }
-                                }
-                                else //Если логин не найден
-                                {
-                                    WriteToStream(tcpClient.GetStream(), "LOGIN");
-                                    WriteToConsole($"{login}: Not found.");
-                                }
                                 break;
                             }
                         case "REGU": //Register user
                             {
-                                string[] info = data.Substring(5).Split(' ');
-                                string login = info[0];
-                                string pass = info[1];
-
-                                if (dbContext.Users.Where(u => u.Login == login).Count() > 0)
-                                {
-                                    WriteToStream(tcpClient.GetStream(), "USER");
-                                    WriteToConsole($"{login}: Already added.");
-                                }
-                                else
-                                {
-                                    Classes.User newUser = new Classes.User() { Login = login, Password = pass };
-                                    dbContext.Users.Add(newUser);
-                                    dbContext.SaveChanges();
-                                    UpdateDataGrid();
-
-                                    WriteToStream(tcpClient.GetStream(), "OK");
-                                    WriteToConsole("User added successfully.");
-                                }
                                 break;
                             }
                         case "RECG": //Receive groups
                             {
-                                //RECG login
-                                string login = data.Substring(5);
-                                int userId = dbContext.Users.Where(u => u.Login == login).Select(u => u.Id).FirstOrDefault();
-
-                                List<Classes.Group> groups = dbContext.Groups.Where(g => g.UserId == userId).ToList();
-                                string json = JsonConvert.SerializeObject(groups, Formatting.Indented);
-                                WriteToStream(tcpClient.GetStream(), json);
                                 break;
                             }
                         case "RECN": //Receive notes
                             {
-                                //RECN 3
-                                int groupId = Int32.Parse(data.Substring(5));
-
-                                List<Classes.Note> notes = dbContext.Notes.Where(n => n.GroupId == groupId).ToList();
-                                string json = JsonConvert.SerializeObject(notes, Formatting.Indented);
-                                WriteToStream(tcpClient.GetStream(), json);
                                 break;
                             }
                         case "CRTG": //Create group
                             {
-                                //CRTG login
-                                string login = data.Substring(5);
-                                int userId = dbContext.Users.Where(u => u.Login == login).Select(u => u.Id).FirstOrDefault();
-
-                                Classes.Group newGroup = new Classes.Group() { Title = "New group", UserId = userId };
-                                dbContext.Groups.Add(newGroup);
-                                dbContext.SaveChanges();
-                                WriteToConsole($"{login}: Added group");
-                                UpdateDataGrid();
-                                string json = JsonConvert.SerializeObject(newGroup, Formatting.Indented);
-                                WriteToStream(tcpClient.GetStream(), json);
                                 break;
                             }
                         case "SAVG": //Save group
                             {
-                                //SAVG login
-                                string json = data.Substring(5);
-
-                                try
-                                {
-                                    Classes.Group editedGroup = JsonConvert.DeserializeObject<Classes.Group>(json);
-                                    if (editedGroup != null)
-                                    {
-                                        Classes.Group groupOnServer = dbContext.Groups.Where(g => g.Id == editedGroup.Id).FirstOrDefault();
-
-                                        if (editedGroup.Title != groupOnServer.Title)
-                                        {
-                                            groupOnServer.Title = editedGroup.Title;
-                                        }
-
-                                        if (editedGroup.Notes != null && editedGroup.Notes.Count != 0)
-                                        {
-                                            foreach (Classes.Note note in editedGroup.Notes)
-                                            {
-                                                Classes.Note noteOnServer = dbContext.Notes.Where(n => n.Id == note.Id).FirstOrDefault();
-
-                                                if (note.Title != noteOnServer.Title)
-                                                {
-                                                    noteOnServer.Title = note.Title;
-                                                }
-                                                if (note.Description != noteOnServer.Description)
-                                                {
-                                                    noteOnServer.Description = note.Description;
-                                                }
-                                                if (note.IsChecked != noteOnServer.IsChecked)
-                                                {
-                                                    noteOnServer.IsChecked = note.IsChecked;
-                                                }
-                                            }
-                                        }
-                                    }
-                                    dbContext.SaveChanges();
-
-                                    WriteToStream(tcpClient.GetStream(), "OK");
-                                    UpdateDataGrid();
-                                }
-                                catch (Exception ex)
-                                {
-                                    WriteToStream(tcpClient.GetStream(), "ERROR");
-                                    MessageBox.Show(ex.Message);
-                                    WriteToConsole(ex.Message);
-                                }
                                 break;
                             }
                         case "CRTN": //Create note
                             {
-                                //CRTN groupId
-                                int groupId = Int32.Parse(data.Substring(5));
-
-                                Classes.Note newNote = new Classes.Note() { Title = "New note", GroupId = groupId, IsChecked = false };
-                                dbContext.Notes.Add(newNote);
-                                dbContext.SaveChanges();
-                                WriteToConsole($"GroupId_{groupId}: Added note");
-                                UpdateDataGrid();
-                                string json = JsonConvert.SerializeObject(newNote, Formatting.Indented);
-                                WriteToStream(tcpClient.GetStream(), json);
                                 break;
                             }
                         default:
@@ -251,10 +109,9 @@ namespace ToDoListServer
                 catch (Exception ex)
                 {
                     WriteToConsole(ex.Message);
-                }             
+                }
             }
         }
-
 
         //Работа с потоком
         private string ReadFromStream(NetworkStream stream)
@@ -270,29 +127,14 @@ namespace ToDoListServer
             } while (len >= buf.Length);
             return Encoding.Unicode.GetString(allBytes.ToArray(), 0, sum);
         }
+
         private void WriteToStream(NetworkStream stream, string message)
         {
             byte[] buf = Encoding.Unicode.GetBytes(message);
             stream.Write(buf, 0, buf.Length);
         }
 
-
         //DataGrid
-        private void UpdateDataGrid()
-        {
-            Action action = () => DrawDataGrid();
-            this.Invoke(action);
-        }
-        private void DrawDataGrid()
-        {
-            switch (DataGridCounter)
-            {
-                case 0: { DataBox.DataSource = dbContext.Users.ToArray(); break; }
-                case 1: { DataBox.DataSource = dbContext.Groups.ToArray(); break; }
-                case 2: { DataBox.DataSource = dbContext.Notes.ToArray(); break; }
-            }
-        }
-
 
         //Console
         private void WriteToConsole(string message)
