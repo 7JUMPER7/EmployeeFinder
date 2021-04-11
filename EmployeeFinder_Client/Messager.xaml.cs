7455baa2d12cd89ee5bb26e9e1a98baab30df72b
Воробьев Георгий <vorobyov.h@gmail.com>
2021-04-12 02:33:56 +0300

    messanger design

--- EmployeeFinder_Client/Messager.xaml.cs
+++ EmployeeFinder_Client/Messager.xaml.cs
@@ -23,5 +23,10 @@ namespace EmployeeFinder_Client
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
