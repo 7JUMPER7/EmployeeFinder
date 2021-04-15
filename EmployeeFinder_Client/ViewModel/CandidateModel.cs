4b96275836d043e1871aff700d2069137ca08d57
Nihon Ashe <fox55ua@gmail.com>
2021-04-15 17:43:38 +0300

    Дизайн окон

--- EmployeeFinder_Client/ViewModel/CandidateModel.cs
+++ EmployeeFinder_Client/ViewModel/CandidateModel.cs
@@ -15,8 +15,124 @@ namespace EmployeeFinder_Client.ViewModel
         /// </summary>
         public CandidateModel(IMainWindowsCodeBehind codeBehind)
         {
+            NewMessage = true;
             if (codeBehind == null) throw new ArgumentNullException(nameof(codeBehind));
             _MainCodeBehind = codeBehind;
+        }
+
+        //Индикатор нового сообщения
+        public bool NewMessage { get; set; }
+
+        /// <summary>
+        /// Ввод имени
+        /// </summary>
+        private string _InputName;
+        public string InputName
+        {
+            get { return _InputName; }
+            set
+            {
+                _InputName = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(InputName)));
+            }
+        }
+
+        /// <summary>
+        /// Ввод специализации
+        /// </summary>
+        private string _InputSpecialisation;
+        public string InputSpecialisation
+        {
+            get { return _InputSpecialisation; }
+            set
+            {
+                _InputSpecialisation = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(InputSpecialisation)));
+            }
+        }
+
+        /// <summary>
+        /// Ввод возраста
+        /// </summary>
+        private int _InputAge;
+        public int InputAge
+        {
+            get { return _InputAge; }
+            set
+            {
+                _InputAge = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(InputAge)));
+            }
+        }
+
+        /// <summary>
+        /// Ввод города
+        /// </summary>
+        private string _InputCity;
+        public string InputCity
+        {
+            get { return _InputCity; }
+            set
+            {
+                _InputCity = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(InputCity)));
+            }
+        }
+
+        /// <summary>
+        /// Ввод портфолио
+        /// </summary>
+        private string _InputPortfolio;
+        public string InputPortfolio
+        {
+            get { return _InputPortfolio; }
+            set
+            {
+                _InputPortfolio = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(InputPortfolio)));
+            }
+        }
+
+        /// <summary>
+        /// Публикация резюме
+        /// </summary>
+        private RelayCommand _PublicationResumeCommand;
+        public RelayCommand PublicationResumeCommand
+        {
+            get
+            {
+                return _PublicationResumeCommand = _PublicationResumeCommand ??
+                  new RelayCommand(OnPublUC, CanPublUC);
+            }
+        }
+        private bool CanPublUC()
+        {
+            return true;
+        }
+        private void OnPublUC()
+        {
+            //логика
+        }
+
+        /// <summary>
+        /// Удаление резюме
+        /// </summary>
+        private RelayCommand _DeleteResumeCommand;
+        public RelayCommand DeleteResumeCommand
+        {
+            get
+            {
+                return _DeleteResumeCommand = _DeleteResumeCommand ??
+                  new RelayCommand(OnDelUC, CanDelUC);
+            }
+        }
+        private bool CanDelUC()
+        {
+            return true;
+        }
+        private void OnDelUC()
+        {
+            //логика
         }
 
         /// <summary>
